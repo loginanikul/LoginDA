c0834df02aa612ac918267844a94c30c396df29e
anirudha <kulkarni1597@gmail.com>
2025-07-17 17:15:10 +0530

    adding print

--- fifth.py
+++ fifth.py
@@ -13,6 +13,12 @@ ORDER BY total_sales DESC
 """
 
 # Execute Query & Load to DataFrame
+#This comment is after first commit
 df = pd.read_sql_query(query, conn)
 
+print(str)
+
 print(df)
+
+a="hello"
+print(a)
